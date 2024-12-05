7ad206309cb038a48f48cf990e67d21fe546c237
shazaniyu55 <shazaniyu@gmail.com>
2024-12-05 14:43:16 +0100

    final

--- routes/userroutes.js
+++ routes/userroutes.js
@@ -1,5 +1,37 @@
-const express = require("express");
+//my express server imports
+const express = require('express');
 const router = express.Router();
+const multer = require("multer");
+
+//all my controller imports 
+const 
+{
+
+    logIn, 
+    signUp, 
+    message,
+    firebaseLogin,
+ 
+    
+} = require("../controller/usercontroller");
 
 
-router.post();
+const storage = multer.memoryStorage(); // Store files in memory
+const upload = multer({ storage: storage });
+
+//all my routes
+
+
+
+//authentication routes
+router.post("/register",signUp);
+router.post("/login", logIn);
+router.post("/message", message);
+router.post('/firebase-login', firebaseLogin)
+
+
+
+
+
+
+module.exports = router
