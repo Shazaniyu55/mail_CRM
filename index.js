64ec77c41e84d4448be7332d618f7554a7e2f81e
shazaniyu55 <shazaniyu@gmail.com>
2024-12-05 15:45:30 +0100

    final

--- index.js
+++ index.js
@@ -5,11 +5,29 @@ const session = require('express-session');
 const cors = require("cors");
 const mongoose = require("mongoose");
 const authRoute = require("./routes/userroutes");
-
+const path = require('path');
 const port  = 4200;
+const swaggerjsdocs = require('swagger-jsdoc');
+const swaggerui = require("swagger-ui-express")
 require('dotenv').config();
 
 
+const options = {
+    definition:{
+        openapi: '3.0.0',
+        info: {
+            title:" mail-crm",
+            version:"1.0.0"
+        },
+        servers:[
+            {
+                url:'http://localhost:4200/'
+            }
+        ]
+    },
+    apis: ['./index.js']
+}
+const swaggerSpec = swaggerjsdocs(options);
 // mongoose.connect(process.env.MONGODB_CONNECTION)
 // .then(()=>{console.log("Database Connected")})
 // .catch((err)=>{console.log(err)});
@@ -29,10 +47,8 @@ app.use(express.urlencoded({extended: true}));
 app.use(cors());
 
 app.use("/api/auth", authRoute);
+app.use("/api-docs", swaggerui.serve, swaggerui.setup(swaggerSpec));
 
-app.get('/', (req, res)=>{
-    res.send("server working")
-})
 
 app.use((req, res, next)=>{
     res.render('404')
