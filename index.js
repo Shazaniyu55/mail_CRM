e70999df6ae3c1f905e72a39924ad2b66c385016
shazaniyu55 <shazaniyu@gmail.com>
2024-12-06 10:48:46 +0100

    final

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const options = {
         },
         servers:[
             {
-                url:'http://localhost:4200/'
+                url:'https://mail-crm.vercel.app/'
             }
         ]
     },
