b1c825140c8e39a41b4361e3a86a8de7799e2c20
shazaniyu55 <shazaniyu@gmail.com>
2024-12-12 13:38:48 +0100

    new

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.use(session({
 //app.use(express.json());
 app.use(bodyParser.json());
 app.use(express.urlencoded({extended: true}));
-app.use(cors({origin:"http://localhost:3000"}));
+app.use(cors({origin:"http://localhost:3000", methods: "GET, POST"}));
 const swaggerSpec = swaggerjsdocs(options);
 app.use("/api-docs", swaggerui.serve, swaggerui.setup(swaggerSpec, {
     customCss:
